310546d53091b37ea0d0e9f260f82969bcafec1d
yingbo5 <yingbo5@staff.weibo.com>
2024-07-18 14:15:13 +0800

    update in macOS!!! Today is 星期四, 七月 18, 2024 14:15:08

--- v2rayse_sub.sh
+++ v2rayse_sub.sh
@@ -96,11 +96,11 @@ yaml_files=$(ls -S *.yaml | head -n 5)
 count=1
 for file in $yaml_files; do
     echo "拷贝文件：${file}"
-    cp "$file" "./${top_folder_name}/yb_v2rayse_sub$count.yaml"
+    # cp "$file" "./${top_folder_name}/yb_v2rayse_sub${count}.yaml"
     count=$((count + 1))
 done
 
-ls "$top_folder_path/*"
+ls "$top_folder_path"
 
 # 输出结果
 echo "前 5 个文件大小最大的 .yaml 文件已复制到缓存文件夹的 ${top_folder_name} 子文件夹中，并重命名为 yb_v2rayse_sub 加序号。"
