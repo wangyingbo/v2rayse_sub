e9264c602bb0eedfd152a238450e0dc7524b16a9
yingbo5 <yingbo5@staff.weibo.com>
2024-07-29 12:39:52 +0800

    modify sh

--- tglaoshiji.sh
+++ tglaoshiji.sh
@@ -12,11 +12,15 @@ echo "\n"
 # 获取今天的日期
 today=$(date +"%Y_%m_%d")
 current_date=$(date +%Y%m%d)
+# 获取昨天的日期
+yesterday_date=$current_date
 current_time=$(date +"Today is %A, %B %d, %Y %H:%M:%S")
 sub_file_name='tglaoshiji.yaml'
 log_file='tglaoshiji_log.txt'
 year=$(date +"%Y")
 month=$(date +"%-m")
+# 获取当前小时（24小时制）
+current_hour=$(date +"%H")
 
 
 # 检测操作系统类型
@@ -25,16 +29,29 @@ YBDEVICE=""
 if [[ "$OSTYPE" == "darwin"* ]]; then
     YBDEVICE="macOS"
     # 在 macOS 上执行的命令或操作
+    yesterday_date=$(date -v-1d +"%Y%m%d")
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     YBDEVICE="Linux"
     # 在 Linux 上执行的命令或操作
+    yesterday_date=$(date -d "yesterday" +"%Y%m%d")
 else
     YBDEVICE="Unknown"
+    yesterday_date=$(date -d "yesterday" +"%Y%m%d")
 fi
 echo "\n"
 
 
-url="https://tglaoshiji.github.io/nodeshare/${year}/${month}/${current_date}.yaml"
+my_date=$yesterday_date
+if [ $current_hour -ge 18 ]; then
+    echo "当前时间大于下午6点"
+    my_date=$current_date
+else
+    echo "当前时间小于下午6点"
+    my_date=$yesterday_date
+fi
+echo "\n"
+
+url="https://tglaoshiji.github.io/nodeshare/${year}/${month}/${my_date}.yaml"
 
 curl $url > ./$sub_file_name
 
