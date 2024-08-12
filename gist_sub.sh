5f45a0486e0d26f7cfa68aaebd79af8042c4a08b
yingbo5 <yingbo5@staff.weibo.com>
2024-08-12 19:29:59 +0800

    opt

--- gist_sub.sh
+++ gist_sub.sh
@@ -12,6 +12,7 @@ file_paths=(
     "https://gist.github.com/Cyndri/70a6d36206b4e3f71afad2c65aec627e"
     "https://gist.github.com/ye4241/1c93c56cd514a757cb8239b52bae3c68"
     "https://gist.github.com/CKCat/f8fec8ee8322e156a241d4819bbd4f54"
+    "https://gist.github.com/mayojoy/88b2863888eadb08b16ea54316c07717"
 )
 
 gist_config="gist_config"
