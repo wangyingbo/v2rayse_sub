d0eeedb00f6ac7534096ed1ad0afd501e05bf93b
yingbo5 <yingbo5@staff.weibo.com>
2024-08-12 13:40:05 +0800

    opt

---
+++ subconverter.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+./wzdnzd_aggregator.sh -a https://gist.github.com/wangyingbo/eb9075f2dc6be6a41eae7765a7fccae7 -f wzdnzd_aggregator_sub -l wzdnzd_aggregator_sub/wzdnzd_aggregator_log.txt
+./wzdnzd_aggregator.sh -a https://gist.github.com/johnzhang0707/8f88dc294f66a68d9c4d352275a8d52d -f johnzhang0707_sub -l johnzhang0707_sub/johnzhang0707_log.txt
+./wzdnzd_aggregator.sh -a https://gist.github.com/zoecsoulkey/4fb494052c2398bdbd36df8d20fb600e -f zoecsoulkey_sub -l zoecsoulkey_sub/zoecsoulkey_log.txt
