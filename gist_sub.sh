51bd38a4af0dd18b03089aa87411c55e479cd2f1
yingbo5 <yingbo5@staff.weibo.com>
2024-08-13 23:31:11 +0800

    add Surfboardv2ray_Proxy-sorter

--- gist_sub.sh
+++ gist_sub.sh
@@ -70,5 +70,8 @@ curl "http://172.245.30.41/v2ray.txt" > "${gist_config}/172_245_30_41_v2ray.txt"
 curl "http://172.245.30.41/singbox.json" > "${gist_config}/172_245_30_41_singbox.json"
 
 
+# base64订阅: https://github.com/Surfboardv2ray/Proxy-sorter
+curl "https://yun-api.subcloud.xyz/sub?target=clash&url=https%3A%2F%2Fraw.githubusercontent.com%2FSurfboardv2ray%2FProxy-sorter%2Fmain%2Fsubmerge%2Fconverted.txt&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true" "${gist_config}/Surfboardv2ray_Proxy-sorter_clash.yaml"
+
 
 ./after_push.sh "${gist_config}/gist_sub_log.txt"
