64e7ed7c68686eb28004880793ebaeb9d9928689
yingbo5 <yingbo5@staff.weibo.com>
2024-08-14 12:13:15 +0800

    opt

--- wzdnzd_aggregator.sh
+++ wzdnzd_aggregator.sh
@@ -185,9 +185,10 @@ for keyword in "${keywords[@]}"; do
         echo "No link found for $keyword"
     fi
 
-    # if [[ $keyword = 'Surge' ]]; then
-    #     ./remove_surge_illegal.sh
-    # fi
+    if [[ $keyword = 'Surge' ]]; then
+        # ./remove_surge_illegal.sh
+        echo "# update in ${YBDEVICE}!!! ${current_time}" >> "$yb_file_folder/$keyword"
+    fi
 done
 
 
