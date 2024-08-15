2908d4c4ebcbeac5978da0700fcd797b4c10206e
yingbo5 <yingbo5@staff.weibo.com>
2024-08-15 12:10:38 +0800

    opt

--- copy_surge_conf_to_gitee.sh
+++ copy_surge_conf_to_gitee.sh
@@ -11,20 +11,26 @@ source_folder=$(cd "$(dirname "$0")"; pwd)
 
 cd $to_path
 
+echo "\n"
 ./before_pull.sh
+echo "\n"
 
 for source_path in "${source_paths[@]}"; do
+
   echo "source path: ${source_folder}/${source_path}"
+  echo "\n"
 
   to_folder=${source_path%/*}
   file_name=${source_path##*/}
 
   echo "copy to: ${to_folder}/${file_name}"
+  echo "\n"
+
   mkdir -p $to_folder
   cp $source_folder/$source_path $to_folder/$file_name
 
 done
 
 
-
+echo "\n"
 ./after_push.sh copy_to_gitee_log.txt
