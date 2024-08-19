bdd3e0cbc4c7a3153e45965e37c5cc30c132a041
yingbo5 <yingbo5@staff.weibo.com>
2024-08-19 14:32:00 +0800

    opt

--- tglaoshiji.sh
+++ tglaoshiji.sh
@@ -4,9 +4,7 @@
 # link3: https://tglaoshiji.github.io/nodeshare/2024/7/20240726.yaml
 
 
-# git
-git pull --rebase
-echo "\n"
+./before_pull.sh
 
 
 # 获取今天的日期
@@ -15,6 +13,7 @@ current_date=$(date +%Y%m%d)
 # 获取昨天的日期
 yesterday_date=$current_date
 current_time=$(date +"Today is %A, %B %d, %Y %H:%M:%S")
+tglaoshiji_foler='tglaoshiji'
 sub_file_name='tglaoshiji.yaml'
 log_file='tglaoshiji_log.txt'
 year=$(date +"%Y")
@@ -57,29 +56,6 @@ url="https://tglaoshiji.github.io/nodeshare/${year}/7/${my_date}.yaml"
 echo "当前的请求地址是：${url}"
 echo "\n"
 
-curl $url > ./$sub_file_name
+curl $url > $tglaoshiji_foler/$sub_file_name
 
-# log
-if [ -e $log_file ]; then
-    echo " 存在log文件 "
-else
-    touch "" > $log_file
-fi
-echo "${current_time} in ${YBDEVICE}" >> $log_file
-log_num_lines=$(wc -l < "${log_file}")
-if [ "$log_num_lines" -gt 100 ]; then
-  tail -n 100 "${log_file}" > tmp_file
-  mv tmp_file "${log_file}"
-  echo "log文件超过100行，已保留后100行，多余行已删除。"
-else
-  echo "log文件不超过100行，无需处理。"
-fi
-echo "\n"
-
-
-# git 
-git add .
-git commit -m "update in ${YBDEVICE}!!! ${current_time}"
-git pull --rebase
-git push -u origin main
-echo "\n"
+./after_push.sh "${tglaoshiji_foler}/${log_file}"
