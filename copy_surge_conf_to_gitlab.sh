27b00a4ab3fe90aca747d046b9982ef8ec0a7f82
yingbo5 <yingbo5@staff.weibo.com>
2024-08-20 11:19:09 +0800

    opt

--- copy_surge_conf_to_gitlab.sh
+++ copy_surge_conf_to_gitlab.sh
@@ -46,7 +46,7 @@ rm -rf github_config; cp -r $source_folder/github_config github_config
 rm -rf top_size; cp -r $source_folder/top_size top_size
 rm -rf iboxz_free_v2ray_collector; cp -r $source_folder/iboxz_free_v2ray_collector iboxz_free_v2ray_collector
 rm -rf itsyebekhe_HiN_VPN; cp -r $source_folder/itsyebekhe_HiN_VPN itsyebekhe_HiN_VPN
-
+rm -rf v2rayclashnodes; cp -r $source_folder/v2rayclashnodes v2rayclashnodes
 
 echo "\n"
 ./after_push.sh copy_to_gitlab_log.txt
