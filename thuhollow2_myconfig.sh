82c2574fe57ba9ac820d011cb4ed9c15a7cdb490
yingbo5 <yingbo5@staff.weibo.com>
2024-08-21 18:55:59 +0800

    opt

--- thuhollow2_myconfig.sh
+++ thuhollow2_myconfig.sh
@@ -5,7 +5,7 @@
 # 设置 GitHub 仓库 URL 和目标目录
 repo_url="https://github.com/thuhollow2/myconfig"
 # 克隆仓库到目录
-target_folder="thuhollow2_myconfig"
+target_folder="myconfig"
 # 检测操作系统类型
 yb_device=$(./getos.sh)
 
@@ -21,35 +21,49 @@ else
 fi
 echo "\n"
 
+./before_pull.sh
+
 # 克隆仓库的元数据
 git clone --depth=1 --filter=blob:none --sparse "$repo_url"
 
 # 进入仓库目录
-cd myconfig || { echo "Failed to enter the repository directory"; cd ..; exit 1; }
+cd $target_folder || { echo "Failed to enter the repository directory"; cd ..; exit 1; }
 
 # 初始化稀疏检出
 git sparse-checkout init --cone
 
 # 获取所有顶层目录和文件
-folders=$(git ls-tree -d --name-only HEAD)
+folders=($(git ls-tree -d --name-only HEAD))
 files=$(git ls-tree -r HEAD --name-only | grep 'config.yaml')
 
-# 设置稀疏检出，逐一下载顶层文件夹及config.yaml
-for folder in $folders; do
-    echo "folder enum: ${folder}"
-    git sparse-checkout set "$folder"
+# 打印所有文件夹（用于调试）
+echo "Found folders:"
+for folder in "${folders[@]}"; do
+  echo "$folder"
 done
 
-# 下载config.yaml文件
-if [ -n "$files" ]; then
-    git sparse-checkout set $files
-fi
+# # 逐一下载每个文件夹
+# for folder in "${folders[@]}"; do
+#     echo "$folder"
+#     git sparse-checkout set "$folder"
+# done
+
+# # 下载config.yaml文件
+# if [ -n "$files" ]; then
+#     git sparse-checkout set $files
+# fi
+
+# 将所有文件夹和config.yaml文件一次性设置到sparse-checkout中
+git sparse-checkout set $folders $files
 
 # 完成后，显示工作目录下的文件和文件夹
 echo "Downloaded files:"
 ls -R
 
+rm -rf .git
+
 # 返回上一级目录
 cd ..
 
+./after_push.sh "${target_folder}/thuhollow2_myconfig_log.txt"
 
