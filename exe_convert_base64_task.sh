4649b5e2e5083c23c8f3e6ff7f57031202901b7e
yingbo5 <yingbo5@staff.weibo.com>
2024-08-23 14:48:55 +0800

    opt

--- exe_convert_base64_task.sh
+++ exe_convert_base64_task.sh
@@ -8,4 +8,4 @@
 ./convert_base64_url_to_sub.sh -a https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/base64/mix -f convert_base64/itsyebekhe_HiN_VPN/mix -l convert_base64/itsyebekhe_HiN_VPN/mix/itsyebekhe_HiN_VPN_mix_log.txt
 ./convert_base64_url_to_sub.sh -a https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/base64/trojan -f convert_base64/itsyebekhe_HiN_VPN/trojan -l convert_base64/itsyebekhe_HiN_VPN/trojan/itsyebekhe_HiN_VPN_trojan_log.txt
 
-./convert_base64_url_to_sub.sh -a http://103.196.20.127/api/v1/client/subscribe?token=786c59b0e5aea08f534779ec09978124 -f convert_base64/103_196_20_127 -l convert_base64/103_196_20_127/kuai_chi_ji_ba_log.txt
+./convert_base64_url_to_sub.sh -a http://103.196.20.127/api/v1/client/subscribe\?token\=786c59b0e5aea08f534779ec09978124 -f convert_base64/103_196_20_127 -l convert_base64/103_196_20_127/kuai_chi_ji_ba_log.txt
