f7c31f238d4564edaa4cc6221fb83667a222b6e8
yingbo5 <yingbo5@staff.weibo.com>
2024-09-03 19:41:53 +0800

    opt

--- push_to_vv_webdav.sh
+++ push_to_vv_webdav.sh
@@ -1,33 +1,44 @@
 #!/bin/zsh
 
 # WebDAV 信息
-WEBDAV_URL="http://webdav.nps.52wxb.top"
-USERNAME="wyb"
-PASSWORD="Yb123456"
+WEBDAV_URL="https://dav.jianguoyun.com/dav/WebDAV/test" # http://webdav.nps.52wxb.top
+USERNAME="wangyingbo528@163.com" # wyb
+PASSWORD="avahyj2vpiwxpj9g" # Yb123456
 
-# 目标文件夹
+
+
+# WebDAV信息
+WEBDAV_URL="https://dav.jianguoyun.com/dav/"
+USERNAME="wangyingbo528@163.com"
+PASSWORD="avahyj2vpiwxpj9g"
 FOLDER_NAME="wzdnzd_aggregator_sub"
 
 # 检查文件夹是否存在
-if [ -d "$FOLDER_NAME" ]; then
-  echo "开始上传文件夹 $FOLDER_NAME 到 $WEBDAV_URL"
-
-  # 遍历文件夹中的所有文件
-  find "$FOLDER_NAME" -type f | while read -r file; do
-    # 构造 WebDAV 上的目标路径
-    RELATIVE_PATH=$(echo "$file" | sed "s|^$FOLDER_NAME/||")
-    TARGET_URL="$WEBDAV_URL/$RELATIVE_PATH"
-
-    # 创建目标路径中的目录
-    DIR_PATH=$(dirname "$RELATIVE_PATH")
-    curl --user "$USERNAME:$PASSWORD" -X MKCOL "$WEBDAV_URL/$DIR_PATH"
-
-    # 上传文件
-    curl --user "$USERNAME:$PASSWORD" -T "$file" "$TARGET_URL"
-    echo "已上传: $file 到 $TARGET_URL"
-  done
-
-  echo "上传完成！"
-else
+if [ ! -d "$FOLDER_NAME" ]; then
   echo "文件夹 $FOLDER_NAME 不存在"
+  exit 1
 fi
+
+# 使用curl上传文件夹到WebDAV
+upload_to_webdav() {
+  local file_path="$1"
+  local relative_path="${file_path#./}"  # 去掉文件路径的前缀 ./
+  local webdav_path="$WEBDAV_URL$relative_path"
+  
+  echo "上传文件: $file_path -> $webdav_path"
+  
+  curl -u "$USERNAME:$PASSWORD" -T "$file_path" "$webdav_path"
+  
+  if [ $? -ne 0 ]; then
+    echo "上传失败: $file_path"
+  else
+    echo "上传成功: $file_path"
+  fi
+}
+
+# 遍历文件夹并上传
+find "$FOLDER_NAME" -type f | while read file; do
+  upload_to_webdav "$file"
+done
+
+echo "所有文件上传完成。"
