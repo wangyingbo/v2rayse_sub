ce095f3304b9a0d6c2a96c7f8d75ae0dbdf33b08
yingbo5 <yingbo5@staff.weibo.com>
2024-09-03 20:12:29 +0800

    opt

--- push_to_vv_webdav.sh
+++ push_to_vv_webdav.sh
@@ -1,16 +1,18 @@
 #!/bin/zsh
 
 # WebDAV 信息
-WEBDAV_URL="https://dav.jianguoyun.com/dav/WebDAV/test" # http://webdav.nps.52wxb.top
-USERNAME="wangyingbo528@163.com" # wyb
-PASSWORD="avahyj2vpiwxpj9g" # Yb123456
+YB_WEBDAV_URL="https://dav.jianguoyun.com/dav/WebDAV/test/"
+YB_USERNAME=wangyingbo528@163.com
+YB_PASSWORD=avahyj2vpiwxpj9g
 
+# curl --user wangyingbo528@163.com:avahyj2vpiwxpj9g -T getos.sh https://dav.jianguoyun.com/dav/WebDAV/test/getos.sh
 
 
 # WebDAV信息
-WEBDAV_URL="http://webdav.nps.52wxb.top/webdav"
-USERNAME="wyb"
-PASSWORD="Yb123456"
+# YB_WEBDAV_URL="http://webdav.nps.52wxb.top/webdav/"
+# YB_USERNAME="wyb"
+# YB_PASSWORD="Yb123456"
+
 FOLDER_NAME="wzdnzd_aggregator_sub"
 
 # 检查文件夹是否存在
@@ -23,11 +25,13 @@ fi
 upload_to_webdav() {
   local file_path="$1"
   local relative_path="${file_path#./}"  # 去掉文件路径的前缀 ./
-  local webdav_path="$WEBDAV_URL$relative_path"
-  
+  local webdav_path="$YB_WEBDAV_URL$relative_path"
+
+  echo "待上传文件路径：${file_path} 待上传文件名：${relative_path}"
   echo "上传文件: $file_path -> $webdav_path"
+  echo "账号：${YB_USERNAME} 密码：${YB_PASSWORD}"
   
-  curl -u "$USERNAME:$PASSWORD" -T "$file_path" "$webdav_path"
+  curl --user "$YB_USERNAME:$YB_PASSWORD" -T $relative_path $webdav_path
   
   if [ $? -ne 0 ]; then
     echo "上传失败: $file_path"
